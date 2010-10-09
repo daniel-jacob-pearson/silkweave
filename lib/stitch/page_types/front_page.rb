80a4c6cd0803198f3574f031c147ca0c2496ab2c
Daniel Pearson <daniel@nanoo.org>
2010-10-09 21:13:32 +0200

    Small comment rewriting.

--- lib/stitch/page_types/front_page.rb
+++ lib/stitch/page_types/front_page.rb
@@ -4,8 +4,8 @@ module Stitch
   module PageTypes
     # A front page is functionally identical to a plain page, but since it has
     # a different name, it can use a different template. This is handy in real
-    # life, since the front page of many sites looks quite rather different
-    # than the other pages on the site.
+    # life, since the front page of many sites is different enough from the
+    # other pages on the site to require a different HTML structure.
     class FrontPage < PlainPage
     end
   end
